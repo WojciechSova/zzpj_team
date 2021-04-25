2ee0c5d9cf4f067e8a22be3254b4218e7585a7a4
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-04-25 12:09:27 +0200

    Fix typo

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"x
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
