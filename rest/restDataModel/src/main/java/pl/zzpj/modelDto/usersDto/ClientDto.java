f02bf9c36953345f6441bc2d9bd4c8015528f825
224333 Kolanek Patryk <224333@edu.p.lodz.pl>
2021-05-13 22:51:47 +0200

    Fix ClientDto

--- rest/restDataModel/src/main/java/pl/zzpj/modelDto/usersDto/ClientDto.java
+++ rest/restDataModel/src/main/java/pl/zzpj/modelDto/usersDto/ClientDto.java
@@ -2,12 +2,18 @@ package pl.zzpj.modelDto.usersDto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import pl.zzpj.modelDto.CurrencyDto;
 
 import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ClientDto extends AccountDto {
+
+    private String accountNumber;
+    private double accountState;
+    private double debt;
+    private CurrencyDto currency;
 
     public ClientDto(UUID uuid, String login, String password, String firstName, String lastName) {
         super(uuid, login, password, firstName, lastName);
