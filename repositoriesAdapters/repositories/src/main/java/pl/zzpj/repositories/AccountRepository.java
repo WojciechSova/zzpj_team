1e23f71a672b79f1755839cabba1baeaa39aad66
Kacper <kacper.swiercz00@gmail.com>
2021-05-15 17:53:41 +0200

    Add Repository annotations

--- repositoriesAdapters/repositories/src/main/java/pl/zzpj/repositories/AccountRepository.java
+++ repositoriesAdapters/repositories/src/main/java/pl/zzpj/repositories/AccountRepository.java
@@ -1,7 +1,9 @@
 package pl.zzpj.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.zzpj.modelEnt.usersEnt.AccountEnt;
 
+@Repository
 public interface AccountRepository extends JpaRepository<AccountEnt, Long> {
 }
--- repositoriesAdapters/repositories/src/main/java/pl/zzpj/repositories/TransactionRepository.java
+++ repositoriesAdapters/repositories/src/main/java/pl/zzpj/repositories/TransactionRepository.java
@@ -1,7 +1,9 @@
 package pl.zzpj.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.zzpj.modelEnt.TransactionEnt;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<TransactionEnt, Long> {
 }
