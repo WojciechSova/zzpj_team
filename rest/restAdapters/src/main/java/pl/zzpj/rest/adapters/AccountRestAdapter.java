a2e69ae18be58e69f7c87b6f8b92efa2e6185851
bruderooo <szymonjacon@gmail.com>
2021-05-17 14:04:08 +0200

    Add AccountRestAdapter

--- rest/restAdapters/src/main/java/pl/zzpj/rest/adapters/AccountRestAdapter.java
+++ rest/restAdapters/src/main/java/pl/zzpj/rest/adapters/AccountRestAdapter.java
@@ -1,7 +1,21 @@
 package pl.zzpj.rest.adapters;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import pl.zzpj.controller.AccountCRUDUseCase;
+import pl.zzpj.dto.AccountDto;
+import pl.zzpj.rest.mappers.AccountMapper;
 
 @Controller
 public class AccountRestAdapter {
+    private final AccountCRUDUseCase accountCRUDUseCase;
+
+    @Autowired
+    public AccountRestAdapter(AccountCRUDUseCase accountCRUDUseCase) {
+        this.accountCRUDUseCase = accountCRUDUseCase;
+    }
+
+    public void addAccount(AccountDto accountDto) {
+        accountCRUDUseCase.addAccount(AccountMapper.mapToAccount(accountDto));
+    }
 }
