8c20d21fa8b5bc56e00770660397f0163970456b
Kacper <kacper.swiercz00@gmail.com>
2021-06-14 15:24:55 +0200

    Extract filtering last 6 months

--- applicationCore/applicationServices/src/main/java/pl/zzpj/loans/NotTrustedStrategy.java
+++ applicationCore/applicationServices/src/main/java/pl/zzpj/loans/NotTrustedStrategy.java
@@ -1,5 +1,6 @@
 package pl.zzpj.loans;
 
+import pl.zzpj.model.Account;
 import pl.zzpj.model.Transaction;
 
 import java.math.BigDecimal;
@@ -8,15 +9,22 @@ import java.util.List;
 import java.util.Map;
 
 public class NotTrustedStrategy extends LoanCalcStrategy {
+
+    Map<Account, List<Transaction>> inTransactionsByAccount = new HashMap<>();
+    Map<Account, List<Transaction>> outTransactionsByAccount = new HashMap<>();
+
     @Override
     public BigDecimal calculate(List<Transaction> inputTransactions,
                                 List<Transaction> outputTransactions,
                                 List<Transaction> loanTakenTransactions,
                                 List<Transaction> loanPaidTransactions) {
         Map<Transaction, Double> inMap = new HashMap<>();
-        for (Transaction inputTransaction : inputTransactions) {
-            inMap.put(inputTransaction, 0.5);
-        }
+
+
+
+//        for (Transaction inputTransaction : ) {
+//            inMap.put(inputTransaction, 0.5);
+//        }
 
         return calcAvgMonthlyAmount(inMap);
     }
