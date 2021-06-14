b15d59f8f53406c36de56046b98c6d083bd5ae74
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-06-14 21:46:58 +0200

    Add some minor features

--- spa/src/app/main/main.component.ts
+++ spa/src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
 
 @Component({
     selector: 'app-main',
@@ -7,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-    constructor() {
+    constructor(private router: Router,
+                private authService: AuthService) {
     }
 
     ngOnInit(): void {
     }
 
+    logOut(): void {
+        this.authService.signOut();
+        this.router.navigate(['']);
+    }
 }
